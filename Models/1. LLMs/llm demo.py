43bf4a16dd8fec9441655eadfa87352b3d699621
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2026-01-09 01:30:48 +0530

    GenAI

---
+++ Models/1. LLMs/llm demo.py
@@ -0,0 +1,9 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model_name="3.5-turbo")
+
+result = llm.invoke("What is the capital of France?")
+print(result)
